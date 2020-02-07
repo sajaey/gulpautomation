a68e23cd98871f9164263d02550692780de4f7bb
sajaycvrajan <sajaycv@yahoo.co.in>
2020-02-07 13:01:23 +0530

    adding js uglify, rename, concat

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,13 +2,16 @@ import gulp from 'gulp';
 import sass from 'gulp-sass';
 import browserSync from 'browser-sync';
 import imagemin from 'gulp-imagemin';
+import concat from 'gulp-concat';
+import rename from 'gulp-rename';
+import uglify from 'gulp-uglify';
 
 const sassOptions = {outputStyle:'expanded', errLogToConsole:true};
 
 exports.sass = () => {
     gulp.src('./src/scss/styles.scss')
     .pipe(sass(sassOptions))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./dist/css/'))
     .pipe(browserSync.reload({stream:true}))
 };
 
@@ -16,6 +19,16 @@ exports.images = () => {
     gulp.src('./src/images/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.reload({stream:true}))
+};
+
+exports.script = () => {
+    gulp.src('./src/javascript/*')
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('./dist/js/'))
+    .pipe(rename('script.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js/'))
     .pipe(browserSync.reload({stream:true}))
 };
 
@@ -38,6 +51,7 @@ gulp.task('serve', () => {
     gulp.watch('./src/scss/**/*',gulp.series('sass'));
     gulp.watch('./src/images/**/*',gulp.series('images'));
     gulp.watch('./src/*.html',gulp.series('copy'));
+    gulp.watch('./src/javascript/*',gulp.series('script'));
     gulp.watch('./dist/*').on('change',browserSync.reload);
 });
 
